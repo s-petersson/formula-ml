8577871a2a886e50a14c2b622b7b52984b6b6e26
Gabriel Alpsten <galpsten@hotmail.com>
2016-02-04 20:41:59 +0100

    Car: Added simple gas and reverse implementation

--- formula-ml/core/Keyboard.h
+++ formula-ml/core/Keyboard.h
@@ -7,5 +7,9 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 bool isKeyDown(int key);
 
+const unsigned int VK_UP = 265;
+const unsigned int VK_DOWN = 264;
+const unsigned int VK_LEFT = 263;
+const unsigned int VK_RIGHT = 262;
 
 #endif
