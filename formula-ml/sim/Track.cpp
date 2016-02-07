5e4800c38c4e88f769e62c6df2e7b673c9c4d89a
Simon Petersson <peterssonaren@gmail.com>
2016-02-07 11:47:49 +0100

    Change to color vector instead of dynamic array

--- formula-ml/sim/Track.cpp
+++ formula-ml/sim/Track.cpp
@@ -6,13 +6,12 @@ Track::Track() {
     model = new Model("./res/models/spa_circuit.model");
 
     // Per vertex color.
-    float * color = new float[model->getVertices().size()];
-
+	std::vector<float> color;
     for(int i = 0; i < model->getVertices().size(); i += 4) {
-        color[i] = 0.5f; // Red
-        color[i + 1] = 0.5f; // Green
-        color[i + 2] = 0.5f; // Blue
-        color[i + 3] = 1.0f; // Alpha
+		color.push_back(0.5f); // Red
+		color.push_back(0.5f); // Green
+		color.push_back(0.5f); // Blue
+		color.push_back(1.0f); // Alpha
     }
 
     glBindVertexArray(model->getVAO());
@@ -20,13 +19,13 @@ Track::Track() {
     GLuint colorBuffer;
     glGenBuffers(1, &colorBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, color.size(), &color[0], GL_STATIC_DRAW);
 
     glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(1);
 
     glBindVertexArray(0);
-    delete[] color;
+    // delete[] color;
 }
 
 Track::~Track() {
