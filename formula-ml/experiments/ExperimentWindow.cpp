d9dc912672c7ed5506148bdf3c1c346b83f0562e
Gabriel Alpsten <galpsten@hotmail.com>
2016-04-14 14:45:58 +0200

    Added class ExperimentWindow

---
+++ formula-ml/experiments/ExperimentWindow.cpp
@@ -0,0 +1,106 @@
+#include "ExperimentWindow.h"
+#include <core/Keyboard.h>
+
+
+/* ExperimentWindow */
+
+ExperimentWindow::ExperimentWindow(Simulator* simulator) {
+    window = new Window();
+    state = new ExperimentState();
+
+    state->simulator = simulator;
+    window->setState(state);
+
+    add_renderer(new StandardRenderer(simulator));
+}
+ExperimentWindow::~ExperimentWindow() {
+    if (window) delete window;
+    if (state) delete state;
+}
+
+void ExperimentWindow::run() {
+    window->run();
+}
+
+void ExperimentWindow::setNetworkLocation(neat::Network** network, bool delayed_update) {
+    state->network_mutex.lock();
+
+    state->network_location = network;
+    state->delayed_update = delayed_update;
+
+    state->network_mutex.unlock();
+}
+
+void ExperimentWindow::updateNetwork(neat::Network* network) {
+    state->network_mutex.lock();
+
+    if (state->network_buffer) delete state->network_buffer;
+    state->network_buffer = network;
+
+    state->network_mutex.unlock();
+}
+
+void ExperimentWindow::add_renderer(Renderer* r) {
+    state->renderers.push_back(r);
+}
+void ExperimentWindow::clear_renderers() {
+    state->renderers.clear();
+}
+
+
+/* ExperimentState helper class */
+
+ExperimentWindow::ExperimentState::ExperimentState() {
+
+}
+ExperimentWindow::ExperimentState::~ExperimentState() {
+    if (simulator) delete simulator;
+    if (network_buffer) delete network_buffer;
+    if (network_view) delete network_view;
+}
+
+void ExperimentWindow::ExperimentState::reset() {
+    simulator->reset();
+
+    network_mutex.lock();
+    if (network_location && network_buffer) {
+        if (*network_location) delete *network_location;
+        *network_location = network_buffer;
+
+        if (network_view) delete network_view;
+        network_view = new NetworkView(network_buffer);
+
+        network_buffer = nullptr;
+    }
+
+    if (network_location && *network_location) {
+        (*network_location)->reset();
+    }
+    network_mutex.unlock();
+}
+
+void ExperimentWindow::ExperimentState::run(float dt) {
+    // Possibly reset
+    network_mutex.lock();
+    bool update_network = !delayed_update && network_buffer;
+    network_mutex.unlock();
+    if (update_network || isKeyDown(GLFW_KEY_HOME)) {
+        reset();
+    }
+
+    // Update simulator
+    for (int i = 0; i < 10; i++) {
+        if (simulator->has_terminated()) {
+            reset();
+        }
+        simulator->update(dt);
+    }
+
+    // Render
+    for (auto && renderer : renderers) {
+        renderer->render();
+    }
+    if (network_view) {
+        network_view->render();
+    }
+}
---
+++ formula-ml/experiments/ExperimentWindow.h
@@ -0,0 +1,58 @@
+#ifndef EXPERIMENT_STATE
+#define EXPERIMENT_STATE
+
+#include <core/WindowState.h>
+#include <core/gfx/Renderer.h>
+#include <core/util/Camera.h>
+#include <sim/Simulator.h>
+#include <sim/SimulationRenderer.h>
+#include <sim/view/CarView.h>
+#include <sim/view/GridView.h>
+#include <sim/view/TrackView.h>
+#include <core/Window.h>
+#include <experiments/StandardRenderer.h>
+#include <neural/neat/Network.h>
+#include <experiments/NetworkView.h>
+#include <mutex>
+
+
+struct Grid;
+
+class ExperimentWindow {
+public:
+    ExperimentWindow(Simulator* simulator);
+    ~ExperimentWindow();
+
+    void run();
+
+    void add_renderer(Renderer* r);
+    void clear_renderers();
+
+    void setNetworkLocation(neat::Network** network, bool delayed_update); // If called, only before run()
+    void updateNetwork(neat::Network* network); // Must only be called after setNetworkLocation()
+
+private:
+    class ExperimentState : public WindowState {
+    public:
+        ExperimentState();
+        ~ExperimentState();
+
+        void run(float dt);
+        void reset();
+
+        Simulator* simulator;
+
+        bool delayed_update;
+        neat::Network** network_location;
+        neat::Network* network_buffer;
+        std::mutex network_mutex;
+
+        vector<Renderer*> renderers;
+        NetworkView* network_view;
+    };
+
+    Window* window;
+    ExperimentState* state;
+};
+
+#endif
