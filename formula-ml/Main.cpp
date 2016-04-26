507b1660af24627d6ba774fd45ac4bb62e01a6a9
Gabriel Alpsten <galpsten@hotmail.com>
2016-04-26 13:49:03 +0200

    Added code for narrow track experiment

--- formula-ml/Main.cpp
+++ formula-ml/Main.cpp
@@ -13,15 +13,16 @@ int main(void) {
     Experiment* experiment = nullptr;
     int chosen_experiment;
     std::cout   << "Experiments available: "    << std::endl
-                << "(1) Manual Control"         << std::endl
-                << "(2) Fixed Midline"          << std::endl
-                << "(3) NEAT with curve data"   << std::endl
-				<< "(4) Loaded NEAT generation with curve data" << std::endl
-                << "(5) XOR "                   << std::endl
-                << "(6) Multiple tracks "       << std::endl
-                << "(7) Alternating tracks "    << std::endl
-                << "(8) Simple track, 90_10_r"  << std::endl
-                << "(9) Shortest distance"      << std::endl
+                << "(1)  Manual Control"        << std::endl
+                << "(2)  Fixed Midline"         << std::endl
+                << "(3)  NEAT with curve data"  << std::endl
+				<< "(4)  Loaded NEAT generation with curve data" << std::endl
+                << "(5)  XOR "                  << std::endl
+                << "(6)  Multiple tracks "      << std::endl
+                << "(7)  Alternating tracks "   << std::endl
+                << "(8)  Simple track, 90_10_r" << std::endl
+                << "(9)  Shortest distance"     << std::endl
+                << "(10) Shortest distance"     << std::endl
                 << "Input the number you want to run: ";
     std::cin >> chosen_experiment;
     switch (chosen_experiment) {
@@ -192,6 +193,43 @@ int main(void) {
                 e->ai_settings = ai_settings;
             }
             break;
+        case 10: // Narrow track
+        {
+            NeatCurveDataExperiment* e = new NeatCurveDataExperiment();
+            experiment = e;
+
+
+            SimulatorSettings sim_settings = SimulatorSettings();
+            sim_settings.track_path = new string("./res/models/circuit_narrow.model"); // Change to an even narrower track
+            sim_settings.completeTrack = true;
+            sim_settings.termination_distance = 5200.f;
+            sim_settings.max_time = 800.f;
+
+            sim_settings.car_speed_max = 50.f;
+            sim_settings.car_speed_initial = 50.f;
+            sim_settings.min_avg_speed = 3.f;
+            sim_settings.avg_speed_excemption_distance = 140.f;
+
+            e->sim_settings = sim_settings;
+
+
+            AiSettings ai_settings = AiSettings();
+            ai_settings.angle_to_line = true;
+            ai_settings.distance_to_middle = true;
+            ai_settings.distance_to_edges = true;
+            ai_settings.speed = true;
+            ai_settings.curve_data = true;
+            ai_settings.curve_data_sum_absolutes = true;
+
+            ai_settings.nbr_of_curve_points = 10;
+            ai_settings.curve_point_spacing = 5.f;
+            ai_settings.curve_point_spacing_incremental_percentage = 0.3f;
+
+            ai_settings.output_speed = true;
+
+            e->ai_settings = ai_settings;
+        }
+        break;
 		default:
 			experiment = new ManualControl();
 			break; 
