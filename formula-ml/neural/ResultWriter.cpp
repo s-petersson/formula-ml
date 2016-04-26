7671ea0f0032b06dd16afb1fb5e0c7d1743206d2
Daniel Eineving <Daniel Eineving>
2016-04-26 09:35:04 +0200

    Made saving work if computer settings are correct

--- formula-ml/neural/ResultWriter.cpp
+++ formula-ml/neural/ResultWriter.cpp
@@ -6,7 +6,7 @@ using namespace std;
 using namespace neural;
 using namespace neat;
 
-string headers = "Generation,Best fitness,Best distance,Best time,Avg fitness,Worst fitness,Species";
+string headers = "Generation,Best fitness,Avg fitness,Worst fitness,Species,Pool size";
 
 
 ResultWriter::ResultWriter(std::string input) {
@@ -24,9 +24,27 @@ ResultWriter::~ResultWriter() {
 }
 
 void ResultWriter::addNewGeneration(int generation, neat::Pool pool) {
-	double best_fitness, best_distance, best_time, sum_fitness, worst_fitness;
-	int species;
+	double best_fitness = 0; 
+	double sum_fitness = 0;
+	double worst_fitness = 0;
+	int species=0;
+	ostringstream genome_values;
+	for (int s = 0; s < pool.species.size(); s++) {
+		species++;
+		Species species = pool.species.at(s);
+		for (int g = 0; g < species.genomes.size(); g++) {
+			Genome genome = species.genomes.at(g);
+			if (genome.fitness > best_fitness) {
+				best_fitness = genome.fitness;
+			}
+			genome_values << "," << genome.fitness;
+			sum_fitness += genome.fitness;
+			if (worst_fitness > genome.fitness || worst_fitness == 0) {
+				worst_fitness = genome.fitness;
+			}
+		}
+	}
 
-	file << generation << endl;
-	//file << pool.maxFitness <<","<<pool.
+	file << generation << "," << best_fitness << "," << sum_fitness / Population << "," << worst_fitness << "," << species << "," << Population;
+	file <<	genome_values.str() << endl;
 }
