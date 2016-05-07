2c1e6d13c6f07dbbc047e57fe33e9c7e878449a4
Martin Nilsson <martin.lennart.nilsson@gmail.com>
2016-05-07 13:26:08 +0200

    Tweak XOR experiment.

--- formula-ml/experiments/xor/XORExperiment.cpp
+++ formula-ml/experiments/xor/XORExperiment.cpp
@@ -5,10 +5,66 @@
 
 using namespace neat;
 using namespace std;
+
+float evaluate_xor(neat::Network* network, bool print) {
+	// Network IO init
+	neural::NetworkIO *in, *out;
+	in = new neural::NetworkIO;
+	out = new neural::NetworkIO;
+	in->value_count = 2;
+	in->values = new float[in->value_count];
+	out->value_count = 1;
+	out->values = new float[out->value_count];
+
+	float fitness = 0.0f;
+	network->reset();
+	in->values[0] = 0.0f;
+	in->values[1] = 0.0f;
+	network->fire(*in, *out);
+	fitness += (1.0f - out->values[0]);
+	if (print) printf("%f XOR %f -> %f --- Score: %f \n", 
+		in->values[0], in->values[1], out->values[0], (1.0f - out->values[0]));
+
+	network->reset();
+	in->values[0] = 0.0f;
+	in->values[1] = 1.0f;
+	network->fire(*in, *out);
+	fitness += out->values[0];
+	if (print) printf("%f XOR %f -> %f --- Score: %f \n",
+		in->values[0], in->values[1], out->values[0], out->values[0]);
+
+
+	network->reset();
+	in->values[0] = 1.0f;
+	in->values[1] = 0.0f;
+	network->fire(*in, *out);
+	fitness += out->values[0];
+	if (print) printf("%f XOR %f -> %f --- Score: %f \n",
+		in->values[0], in->values[1], out->values[0], out->values[0]);
+
+	network->reset();
+	in->values[0] = 1.0f;
+	in->values[1] = 1.0f;
+	network->fire(*in, *out);
+	fitness += (1.0f - out->values[0]);
+	if (print) printf("%f XOR %f -> %f --- Score: %f \n",
+		in->values[0], in->values[1], out->values[0], (1.0f - out->values[0]));
+
+	fitness = fitness * fitness;
+
+	// Cleanup and return
+	delete[] in->values;
+	delete[] out->values;
+	delete in;
+	delete out;
+	return fitness;
+}
+
 XORExperiment::XORExperiment()
 {
 
     Config::set_config(2, 1);
+	Config::InitialStructure = true;
 
     trainer = new Trainer();
    
@@ -22,6 +78,9 @@ XORExperiment::XORExperiment()
 
     trainer->on_new_best = [](EvaluationResult evaluationResult, Genome genome){
         cout << "New best fitness: " << evaluationResult.fitness << endl;
+		auto n = new Network(genome.genes);
+		evaluate_xor(n,true);
+		delete n;
     };
 }
 
@@ -37,45 +96,8 @@ void XORExperiment::run() {
 }
 
 EvaluationResult XOREvaluator::evaluate_network(neat::Network* network) {
-    // Network IO init
-    neural::NetworkIO *in, *out;
-    in = new neural::NetworkIO;
-    out = new neural::NetworkIO;
-    in->value_count = 2;
-    in->values = new float[in->value_count];
-    out->value_count = 1;
-    out->values = new float[out->value_count];
     
-    float fitness = 0.0f;
-
-    in->values[0] = 0.0f;
-    in->values[1] = 0.0f;
-    network->fire(*in, *out);
-    fitness += (1.0f - out->values[0]);
-
-    in->values[0] = 0.0f;
-    in->values[1] = 1.0f;
-    network->fire(*in, *out);
-    fitness += out->values[0];
-
-    in->values[0] = 1.0f;
-    in->values[1] = 0.0f;
-    network->fire(*in, *out);
-    fitness += out->values[0];
-
-    in->values[0] = 1.0f;
-    in->values[1] = 1.0f;
-    network->fire(*in, *out);
-    fitness += (1.0f - out->values[0]);
-
-    fitness = fitness * fitness;
-
-    // Cleanup and return
-    delete[] in->values;
-    delete[] out->values;
-    delete in;
-    delete out;
     EvaluationResult evaluationResult = EvaluationResult();
-    evaluationResult.fitness = fitness;
+    evaluationResult.fitness = evaluate_xor(network, false);
     return evaluationResult;
 }
