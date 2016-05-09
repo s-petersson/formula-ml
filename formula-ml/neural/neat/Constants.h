a9fc6e4ca63bcf9817ac9374e0211da8fe38efa3
Simon Petersson <Peterssonaren@gmail.com>
2016-05-09 12:25:02 +0200

    Modify neat constants to perform experiments

--- formula-ml/neural/neat/Constants.h
+++ formula-ml/neural/neat/Constants.h
@@ -15,24 +15,21 @@ namespace neat {
 
         static std::function<float(float x)> sigmoid;
     };
-    const int Population = 500;
     const float DeltaDisjoint = 2.0f;
     const float DeltaWeights = 0.4f;
     const float DeltaThreshold = 1.0f;
 
-    const int StaleSpecies = 15;
-
-
+    const int Population = 1000;
     const float MutateConnectionsChance = 0.70f;
     const float PerturbChance = 0.90f;
     const float CrossoverChance = 0.75f;
     const float LinkMutationChance = 0.50f;
-    const float NodeMutationChance = 0.30f;
+    const float NodeMutationChance = 0.40f;
     const float BiasMutationChance = 0.40f;
     const float StepSize = 0.1f;
     const float DisableMutationChance = 0.4f;
-
     const float EnableMutationChance = 0.2f;
+    const int StaleSpecies = 20;
 
     const int MaxNodes = 1000000;
 }
